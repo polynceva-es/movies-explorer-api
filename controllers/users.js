ffbc16312a46f7d60d7a7d6994af84bf21c642af
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-05-28 21:16:08 +0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -45,6 +45,8 @@ const updateUser = (params, req, res, next) => {
     .catch((err) => {
       if (err instanceof mongoose.Error.ValidationError) {
         next(new BadRequestError('Переданы некорректные данные при обновлении профиля'));
+      } else if (err.code === 11000) {
+        next(new ConflictError('Пользователь с таким email уже существует'));
       } else { next(err); }
     });
 };
